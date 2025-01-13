194783140c361045101207a5976b80b65bcc0f49
akulajayaram <akhulaghanasai@ascent-hr.com>
2025-01-14 01:12:18 +0530

    env config fixes

--- src/database/typeorm/typeOrm.config.ts
+++ src/database/typeorm/typeOrm.config.ts
@@ -3,17 +3,15 @@ import { resolve } from 'path';
 import { getEnvPath } from 'src/core/utils/env.helper';
 import { DataSourceOptions } from 'typeorm';
 
-const envFilePath: string = getEnvPath(resolve(__dirname, '../..', 'core/env'));
+const envFilePath: string = getEnvPath(resolve(__dirname, '../../..', 'env'));
 config({ path: envFilePath });
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host:
-    process.env.DATABASE_HOST ||
-    'database.czk6cc8c8vrb.ap-south-1.rds.amazonaws.com',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  database: process.env.DATABASE_NAME || 'postgres',
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'asdf1234',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   entities: [`dist/**/*.entity.{ts,js}`],
   // migrations: ['dist/database/migration/history/*.js'],
   logger: 'simple-console',
